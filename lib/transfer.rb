6aca4dd44e1d52962ce1d8c16df6eaf58b427b03
Araba Taylor <araba.taylor20@gmail.com>
2017-11-19 14:41:11 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,6 +21,7 @@ attr_accessor :sender, :receiver, :status, :amount
   def execute_transaction
     @receiver.balance += @amount
     @sender.balance -= @amount
+    @status = "complete"
   end
 
 end
