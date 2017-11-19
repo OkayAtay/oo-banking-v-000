9eac08539d9f26dc032db4b1d9118fe9ca1a0e5c
Araba Taylor <araba.taylor20@gmail.com>
2017-11-19 14:33:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,6 +3,7 @@ class Transfer
 attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver)
+    @status="pending"
     @sender=sender
     @receiver=receiver
     @amount=amount
