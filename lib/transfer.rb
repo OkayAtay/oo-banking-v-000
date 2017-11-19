e37d299334db08fdf630c21b55787e040dd94346
Araba Taylor <araba.taylor20@gmail.com>
2017-11-19 15:10:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Transfer
 
-attr_accessor :sender, :receiver, :status, :amount, :counter
+attr_accessor :sender, :receiver, :status, :amount
 
 
   def initialize(sender, receiver, amount)
@@ -21,10 +21,9 @@ attr_accessor :sender, :receiver, :status, :amount, :counter
   end
 
   def execute_transaction
-    if self.valid? == true && @counter == 0
+    if self.valid? == true 
       @receiver.balance += @amount
       @sender.balance -= @amount
-      @counter += 1;
       @status = "complete"
     else
       @status = "rejected"
