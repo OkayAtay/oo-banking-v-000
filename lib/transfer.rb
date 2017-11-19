61eee5efc70b50a3baf6b35d661b01a0b50b9de9
Araba Taylor <araba.taylor20@gmail.com>
2017-11-19 15:43:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,7 +34,8 @@ attr_accessor :sender, :receiver, :status, :amount, :counter
 
   def reverse_transfer
     if @status == "complete"
-
+      @receiver.balance -= @amount
+      @sender.balance += @amount
     end
 
   end
