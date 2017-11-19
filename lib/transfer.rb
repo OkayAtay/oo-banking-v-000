95855bf0bf94deb91fdd717cea7431d066eaf1d9
Araba Taylor <araba.taylor20@gmail.com>
2017-11-19 15:03:51 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ attr_accessor :sender, :receiver, :status, :amount, :counter
   end
 
   def valid?
-    if   @sender.valid? && @receiver.valid? 
+    if   @sender.valid? && @receiver.valid?
       true
     else
       false
